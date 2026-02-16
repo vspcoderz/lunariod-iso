c20d7aecfd9e14898606f68b9dcb58b9c7a87a6f
async-zenodev <async.zenodev@gmail.com>
2026-02-16 20:04:06 +0530

    fix: add --privileged to CI container, init keyring, remove gshadow

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -16,7 +16,6 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.bash_profile"]="0:0:644"
   ["/usr/local/bin/lunariod-menu"]="0:0:755"
