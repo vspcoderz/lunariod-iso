46188904633760b1ef5e496999f1ff1073b87e38
async-zenodev <async.zenodev@gmail.com>
2026-02-17 11:45:59 +0530

    fix: rebrand MOTD, fix .automated_script.sh perms, switch UEFI to GRUB for banner

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -9,7 +9,7 @@ iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
-           'uefi.systemd-boot')
+           'uefi-x64.grub.esp')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
@@ -17,6 +17,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.bash_profile"]="0:0:644"
   ["/usr/local/bin/lunariod-menu"]="0:0:755"
   ["/usr/local/bin/lunariod-install"]="0:0:755"
